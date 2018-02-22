3aeee11e9735094d6531c06d5c91c324433ffa70
alferguson <alferguson@berkeley.edu>
2018-02-22 11:17:36 -0800

    ugh, really screwed up urlify

---
+++ chp01/1.4palindromePermutation.js
@@ -0,0 +1,35 @@
+// Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+// A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+// is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+// EXAMPLE
+// Input: Tact Coa
+// Output: True (permutations: "taco cat", "atco eta", etc.)
+// Hints: #106, #121, #134, #136 
+
+function palindromePerm(str) {
+    let newStr = str.split(" ").join("");
+    let oddLetterCompare;
+    let oddLetterCounter;
+    // check if string is even length not including spaces
+    for (let i = 0; i < newStr.length; i++) {
+        let counter = 0;
+        for (let j = 0; j < newStr.length; j++) {
+            if (newStr[i] === newStr[j]) {
+                counter++;
+            };
+        }
+        if (counter % 2 !== 0) {
+            oddLetterCounter++;
+            if (oddLetterCounter > 1 && oddLetterCompare !== newStr[i]) {
+                return false;
+            }
+            oddLetterCompare = newStr[i];
+        }
+    }
+    return true;
+}
+
+console.log(palindromePerm("Heyo"));
+console.log(palindromePerm("wywy"));
+console.log(palindromePerm("ninix"));
