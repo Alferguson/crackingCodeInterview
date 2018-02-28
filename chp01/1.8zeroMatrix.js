8a57bfd1d371f46e4cac53fa1ad3fec64d5c6bf8
alferguson <alferguson@berkeley.edu>
2018-02-28 12:56:49 -0800

    zero matrix update

---
+++ chp01/1.8zeroMatrix.js
@@ -0,0 +1,15 @@
+// Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+// column are set to 0. 
+
+function zeroMatrix(matrix) {
+    let matrixObj = {};
+    for (let i = 0; i < matrix.length; i++) {
+        
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                
+            }
+        }
+        
+    }
+}
