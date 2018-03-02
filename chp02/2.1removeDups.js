052e906c58baf13e3dd2430d9b66641641f08172
alferguson <alferguson@berkeley.edu>
2018-03-02 12:05:42 -0800

    small changes

---
+++ chp02/2.1removeDups.js
@@ -0,0 +1,8 @@
+// Remove  Dups! Write code to remove duplicates from an unsorted linked list.
+// FOLLOW UP
+// How would you solve this problem if a temporary buffer is not allowed?
+
+const LinkedList = function(value) {
+    this.value = value;
+    this.next = null;
+}
